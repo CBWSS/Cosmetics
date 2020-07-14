006de789dea570b8b2a14fb2e9424eb304e12481
CheatBreakerCapes <64689493+CheatBreakerCapes@users.noreply.github.com>
2020-07-14 10:09:48 +0500

    Update cosmetics.php

--- cosmetics.php
+++ cosmetics.php
@@ -1,5 +1,7 @@
 <?php
 if (isset($_GET['uuid'])) {
-    echo '{"uuid":"' . $_GET['uuid'] . '","username":null,"cosmetics":{"protected":{"name":"protected","type":"cape","scale":0.16,"active":true,"resourceLocation":"aac"},"purple":{"name":"purple","type":"dragon_wings","scale":0.13,"active":true,"resourceLocation":"purple"}}}';
+    echo '{"uuid":"' . $_GET['uuid'] . '","username":null,"cosmetics":{"bluefoglogo":
+{"name":"bluefoglogo","type":"cape","scale":0.16,"active":true,"resourceLocation":"bluefoglogo"},"bluefogwings":
+{"name":"bluefogwings","type":"dragon_wings","scale":0.10,"active":false,"resourceLocation":"bluefogwings"}}}';
 }
 ?>
