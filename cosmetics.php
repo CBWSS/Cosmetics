9be33f7e959547ef04c13f9721b0f0569f5ace2e
CheatBreakerCapes <64689493+CheatBreakerCapes@users.noreply.github.com>
2020-09-06 02:29:49 +0500

    Delete cosmetics.php

--- cosmetics.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET['uuid'])) {
-    echo '{"uuid":"' . $_GET['uuid'] . '","username":null,"cosmetics":{"bluefoglogo":
-{"name":"bluefoglogo","type":"cape","scale":0.16,"active":true,"resourceLocation":"bluefoglogo"},"bluefogwings":
-{"name":"bluefogwings","type":"dragon_wings","scale":0.10,"active":false,"resourceLocation":"bluefogwings"}}}';
-}
-?>
